7a7d27c85bb7c002e34065329d89425dfa4da3ff
teerakanotk <teerakan.otk@outlook.com>
2025-11-06 18:28:59 +0700

    feat(next.config.mjs): Add config output standalone to reduce Docker image size
    Enables 'output: "standalone"' in Next.js configuration.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  // Recommend for docker build
+  output: "standalone",
 };
 
 export default nextConfig;
